98d1bf2082ad4ee86923041e6966d03636727e1e
Samuel Santana <samuel_sankys@hotmail.com>
2023-07-24 23:40:01 -0300

    feat(back): add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,15 @@
-const job = require('./jobs/loadData')
 require('dotenv').config({
   path: process.env.NODE_ENV === 'production' ? '.env' : '.env.development',
 })
 
+const job = require('./jobs/loadData')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
